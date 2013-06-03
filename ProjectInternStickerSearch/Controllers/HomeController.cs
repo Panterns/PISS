fc783eaa59e0eff68c275a7dc44d5e4aba5be1e7
Seth Ruji <srujijunk@gmail.com>
2013-06-03 09:34:34 -0400

    all my stuff for PISS

---
+++ ProjectInternStickerSearch/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Transactions;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using ProjectInternStickerSearch.Filters;
+using ProjectInternStickerSearch.Models;
+
+namespace ProjectInternStickerSearch.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        public static string username;
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Group()
+        {
+            FormsAuthentication.SetAuthCookie(null, true);
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SetAuthCookie(null, true);
+            username = null;
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult LoginCall(string UserName, string Password)
+        {
+            Login loginModelObject = new Login();
+            
+            if (0 == loginModelObject.LogIn(UserName, Password))
+            {
+                FormsAuthentication.SetAuthCookie(UserName, true);
+                username = UserName;
+                 return View("Profile");
+            }
+            else
+            {
+                 return View("Group");
+            }
+        }
+    }
+}
---
+++ ProjectInternStickerSearch/Models/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ProjectInternStickerSearch.Models
+{
+    public class Login
+    {
+        public int LogIn(string username, string password)
+        {
+            using (var connection = new SqlConnection("Server=qadev0db-dev\\dev,50400;Database=QA;User Id=uPSavkovich;Password=uPS11137350!;"))
+            {
+                connection.Open();
+                var parms = new DynamicParameters();
+                parms.Add("@Username", username);
+                parms.Add("@Password", password);
+                parms.Add("@Result", 0, DbType.Int32, ParameterDirection.Output);
+
+                var login = SqlMapper.Query<int>(connection, "Intern.ConfirmLogIn",
+                                                          commandType: CommandType.StoredProcedure,
+                                                          param: parms);
+                System.Console.WriteLine("hello");
+                System.Console.WriteLine(login);
+                return parms.Get<int>("@Result");
+            }
+        }
+    }
+}
